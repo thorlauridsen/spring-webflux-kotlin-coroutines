6eb5f248fa7ab513bc87cacb86cf903aea978307
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-16 23:01:15 +0100

    docs: Update kdocs for GatewayApplication

--- apps/gateway/src/main/kotlin/com/github/thorlauridsen/GatewayApplication.kt
+++ apps/gateway/src/main/kotlin/com/github/thorlauridsen/GatewayApplication.kt
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
+/**
+ * Main entry point for the gateway application.
+ * We need to enable configuration properties for [GatewayConfig].
+ * This ensures that Spring Boot will bind the configuration
+ * properties from application.yml to the [GatewayConfig] class.
+ */
 @SpringBootApplication
 @EnableConfigurationProperties(
 	GatewayConfig::class,
