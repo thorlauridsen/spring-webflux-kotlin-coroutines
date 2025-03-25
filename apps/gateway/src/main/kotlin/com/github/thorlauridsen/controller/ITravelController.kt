42201e1dd1a106de7425cb6cbb85a314b9c6ff18
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-25 08:44:32 +0100

    refac: Update TRAVEL_BASE_ENDPOINT from /travel to /travel/details

--- apps/gateway/src/main/kotlin/com/github/thorlauridsen/controller/ITravelController.kt
+++ apps/gateway/src/main/kotlin/com/github/thorlauridsen/controller/ITravelController.kt
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
-const val TRAVEL_BASE_ENDPOINT = "/travel"
+const val TRAVEL_BASE_ENDPOINT = "/travel/details"
 
 /**
  * Travel controller interface.
