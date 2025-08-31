09b1cc4afebcaaf590b87fd642d5de7201d4987d
Thor Tudal Lauridsen <30320530+thorlauridsen@users.noreply.github.com>
2025-08-31 18:08:09 +0200

    build: Use Kotlin DSL accessors in build.gradle.kts files (#65)
    Take advantage of the features of Kotlin DSL by using accessors in `build.gradle.kts` files.

--- modules/persistence/build.gradle.kts
+++ modules/persistence/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-import org.springframework.boot.gradle.tasks.run.BootRun
-
 plugins {
     kotlin("plugin.spring") version local.versions.kotlin
     alias(local.plugins.springboot)
@@ -17,9 +14,9 @@ dependencies {
 
 // Disabling bootJar and bootRun is necessary for a subproject/module
 // that uses the Spring Boot plugin but is not supposed to be executable.
-tasks.named<BootJar>("bootJar") {
+tasks.bootJar {
     enabled = false
 }
-tasks.named<BootRun>("bootRun") {
+tasks.bootRun {
     enabled = false
 }
